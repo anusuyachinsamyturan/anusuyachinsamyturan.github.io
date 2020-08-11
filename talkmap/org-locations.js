fb47c1794bfdb57e9f92bb86835a3b8c3f622c1d
anusuyachinsamyturan <69392946+anusuyachinsamyturan@users.noreply.github.com>
2020-08-11 12:00:14 +0200

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,277 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Chicago USA",
+    41.8755616,
+    -87.6244212
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "San Diego; USA",
+    32.7174209,
+    -117.1627714
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Philadelphia USA",
+    39.9527237,
+    -75.1635262
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Philadelphia USA ",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "Philadelphia USA;",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "Philadelphia; USA",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "San Francisco USA ",
+    37.7790262,
+    -122.4199061
+  ],
+  [
+    "Indiana USA ",
+    40.3270127,
+    -86.1746933
+  ],
+  [
+    "South Africa",
+    -28.8166236,
+    24.991639
+  ],
+  [
+    "Pretoria South Africa",
+    -25.7459374,
+    28.1879444
+  ],
+  [
+    "Nelspruit South Africa",
+    -25.481944,
+    30.984722
+  ],
+  [
+    " South Africa",
+    -28.8166236,
+    24.991639
+  ],
+  [
+    "Rustenburg South Africa ",
+    -25.665655,
+    27.241448
+  ],
+  [
+    "Buenos Aires Argentina ",
+    45.4838459,
+    9.2147675
+  ],
+  [
+    "Nairobi Kenya ",
+    -1.2832533,
+    36.8172449
+  ],
+  [
+    "Tokyo Japan ",
+    55.7746222,
+    37.6326806
+  ],
+  [
+    "Jena Germany ",
+    50.9281717,
+    11.5879359
+  ],
+  [
+    "Cape Town South Africa ",
+    -33.928992,
+    18.417396
+  ],
+  [
+    "Cape Town  South Africa  ",
+    -33.928992,
+    18.417396
+  ],
+  [
+    "Pretoria  South Africa ",
+    -25.7459374,
+    28.1879444
+  ],
+  [
+    "Cape Town South Africa",
+    -33.928992,
+    18.417396
+  ],
+  [
+    "Boca Raton Florida USA",
+    26.3586885,
+    -80.0830984
+  ],
+  [
+    "Grahamstown  South Africa ",
+    -33.3070589,
+    26.5272648
+  ],
+  [
+    " South Africa ",
+    -28.8166236,
+    24.991639
+  ],
+  [
+    "Durban South Africa",
+    -29.861825,
+    31.009909
+  ],
+  [
+    "NY USA",
+    43.1561681,
+    -75.8449946
+  ],
+  [
+    " Grahamstown  South Africa ",
+    -33.3070589,
+    26.5272648
+  ],
+  [
+    "Seoul Korea ",
+    37.5666791,
+    126.9782914
+  ],
+  [
+    "University of Pretoria South Africa",
+    -25.7542559,
+    28.2321043
+  ],
+  [
+    "Ankara Turkey ",
+    39.9207774,
+    32.854067
+  ],
+  [
+    "Izmir Turkey ",
+    38.4147331,
+    27.1434119
+  ],
+  [
+    "Afyon Turkey ",
+    38.6852729,
+    30.6427411
+  ],
+  [
+    "Grahamstown South Africa",
+    -33.3070589,
+    26.5272648
+  ],
+  [
+    "Pretoria ",
+    -25.7459374,
+    28.1879444
+  ],
+  [
+    "Stellenbosch South Africa ",
+    -33.934444,
+    18.869167
+  ],
+  [
+    "Bristol England ",
+    51.4538022,
+    -2.5972985
+  ],
+  [
+    "Bonn Germany",
+    50.735851,
+    7.10066
+  ],
+  [
+    " South Africa  ",
+    -28.8166236,
+    24.991639
+  ],
+  [
+    "Sabadell Spain ",
+    41.5421013,
+    2.1138977
+  ],
+  [
+    " Durban South Africa ",
+    -29.861825,
+    31.009909
+  ],
+  [
+    "Vienna ",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Korea ",
+    36.638392,
+    127.6961188
+  ],
+  [
+    "China",
+    35.000074,
+    104.999927
+  ],
+  [
+    "Paris France ",
+    48.8566969,
+    2.3514616
+  ],
+  [
+    "France",
+    46.603354,
+    1.8883335
+  ],
+  [
+    "Betty\u2019s Bay South Africa",
+    -34.353611,
+    18.917778
+  ],
+  [
+    "Turin Italy ",
+    45.0677551,
+    7.6824892
+  ],
+  [
+    "Mendoza Argentina ",
+    -34.78719615,
+    -68.43807123826872
+  ],
+  [
+    "Bangalore India",
+    12.9791198,
+    77.5912997
+  ],
+  [
+    "Alice Springs Australia ",
+    -23.6983884,
+    133.8812885
+  ],
+  [
+    "Stellenbosch  South Africa",
+    -33.934444,
+    18.869167
+  ],
+  [
+    "Paris France",
+    48.8566969,
+    2.3514616
+  ],
+  [
+    "Prague Czech",
+    50.0864234,
+    14.4156772
+  ],
+  [
+    "Adelaide Australia ",
+    -34.9281805,
+    138.5999312
+  ],
+  [
+    "Nelson Mandela Metropolitan University ",
+    -29.1106677,
+    26.1864764
   ]
 ];
